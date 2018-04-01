298bcbb2febacf192df442173e08aca0e47704ff
Bryton Hall <email@bryton.io>
2018-04-01 01:33:27 -0400

    temporarily comment out messaging system for testing deployment

--- src/main/java/com/revature/caliber/BatchRepositoryServiceApplication.java
+++ src/main/java/com/revature/caliber/BatchRepositoryServiceApplication.java
@@ -4,12 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.revature.caliber.model.SimpleBatch;
 import com.revature.caliber.service.BatchCompositionMessageService;
 import com.revature.caliber.service.BatchCompositionService;
 
@@ -21,21 +18,21 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-@EnableEurekaClient 
+@EnableDiscoveryClient 
 public class BatchRepositoryServiceApplication {
-//	@Autowired
-//	BatchCompositionMessageService bcms;
+	@Autowired
+	BatchCompositionMessageService bcms;
 	@Autowired
 	BatchCompositionService bcs;
 	public static void main(String[] args) {
 		SpringApplication.run(BatchRepositoryServiceApplication.class, args);
 	}
-	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-			System.out.println(bcs.findAllCurrent());
-		};
-	}
+	// @Bean
+	// public CommandLineRunner runner() {
+	// 	return args -> {
+	// 		System.out.println(bcs.findAllCurrent());
+	// 	};
+	// }
 	@Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
@@ -46,4 +43,3 @@ public class BatchRepositoryServiceApplication {
     }
 	
 }
-
--- src/main/java/com/revature/caliber/service/BatchCompositionMessageService.java
+++ src/main/java/com/revature/caliber/service/BatchCompositionMessageService.java
@@ -1,27 +1,21 @@
 package com.revature.caliber.service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.revature.caliber.model.Address;
-import com.revature.caliber.model.Batch;
-import com.revature.caliber.model.SimpleBatch;
 import com.revature.caliber.model.SimpleNote;
 import com.revature.caliber.model.SimpleTrainee;
 import com.revature.caliber.model.SimpleTrainer;
-import com.revature.caliber.model.Trainer;
 
 @Service
 public class BatchCompositionMessageService {
-	@Autowired
-	private AmqpTemplate rabbitTemplate;
+	// @Autowired
+	// private AmqpTemplate rabbitTemplate;
 	
 	private static final String LIST_TRAINEE_ROUTING_KEY = "eRQ7GaBRnHgGdV9D";
 	private static final String LIST_NOTE_ROUTING_KEY = "cf22J9CGs8V95Rbm";
@@ -29,7 +23,7 @@ public class BatchCompositionMessageService {
 	private static final String TRAINER_ROUTING_KEY = "9xdaX72tPYuz8xDP";
 	private static final String ADDRESS_ROUTING_KEY = "oEKQeav8rcejeYgq";
 	private static final String ADDRESS_LIST_ROUTING_KEY = "Jf2zpP6hPvPHEXsV";
-	private static final String RABBIT_REPO_EXCHANGE = "revature.caliber.repos";
+	private static final String RABBIT_REPO_EXCHANGE = "revature.hydra.repos";
 	/**
 	 * 
 	 * @param trainerId
@@ -39,8 +33,8 @@ public class BatchCompositionMessageService {
 		JsonObject SimpleTrainerRequest = new JsonObject();
 		SimpleTrainerRequest.addProperty("methodName", "findOne");
 		SimpleTrainerRequest.addProperty("trainerId", trainerId);
-		return (SimpleTrainer) rabbitTemplate.convertSendAndReceive(
-				RABBIT_REPO_EXCHANGE, TRAINER_ROUTING_KEY, SimpleTrainerRequest.toString());
+		return null;//(SimpleTrainer) rabbitTemplate.convertSendAndReceive(
+				//RABBIT_REPO_EXCHANGE, TRAINER_ROUTING_KEY, SimpleTrainerRequest.toString());
 		
 	}
 	/**
@@ -52,8 +46,8 @@ public class BatchCompositionMessageService {
 		JsonObject SimpleAddressRequest = new JsonObject();
 		SimpleAddressRequest.addProperty("methodName", "findOne");
 		SimpleAddressRequest.addProperty("addressId", addressId);
-		return (Address) rabbitTemplate.convertSendAndReceive(
-				RABBIT_REPO_EXCHANGE, ADDRESS_ROUTING_KEY, SimpleAddressRequest.toString());
+		return null;//(Address) rabbitTemplate.convertSendAndReceive(
+				//RABBIT_REPO_EXCHANGE, ADDRESS_ROUTING_KEY, SimpleAddressRequest.toString());
 		
 	}
 	/**
@@ -64,8 +58,8 @@ public class BatchCompositionMessageService {
 	public List<Address> sendSimpleAddressListRequest() {
 		JsonObject SimpleAddressListRequest = new JsonObject();
 		SimpleAddressListRequest.addProperty("methodName", "findAll");
-		return (List<Address>) rabbitTemplate.convertSendAndReceive(
-				RABBIT_REPO_EXCHANGE, ADDRESS_LIST_ROUTING_KEY, SimpleAddressListRequest.toString());
+		return null;//(List<Address>) rabbitTemplate.convertSendAndReceive(
+				//RABBIT_REPO_EXCHANGE, ADDRESS_LIST_ROUTING_KEY, SimpleAddressListRequest.toString());
 	}
 	/**
 	 * 
@@ -76,9 +70,9 @@ public class BatchCompositionMessageService {
 		JsonObject SimpleTraineeListRequest = new JsonObject();
 		SimpleTraineeListRequest.addProperty("methodName", "findAllByBatchId");
 		SimpleTraineeListRequest.addProperty("batchId", batchId);
-		List<?> SimpleTraineeList=(List<?>) rabbitTemplate.convertSendAndReceive(RABBIT_REPO_EXCHANGE,
-				LIST_TRAINEE_ROUTING_KEY, SimpleTraineeListRequest.toString());
-		return SimpleTraineeList.stream().map(x->(SimpleTrainee)x).collect(Collectors.toList());
+		// List<?> SimpleTraineeList=(List<?>) rabbitTemplate.convertSendAndReceive(RABBIT_REPO_EXCHANGE,
+		// 		LIST_TRAINEE_ROUTING_KEY, SimpleTraineeListRequest.toString());
+		return null;//SimpleTraineeList.stream().map(x->(SimpleTrainee)x).collect(Collectors.toList());
 	}
 	/**
 	 * 
@@ -89,9 +83,9 @@ public class BatchCompositionMessageService {
 		JsonObject SimpleNoteListRequest = new JsonObject();
 		SimpleNoteListRequest.addProperty("methodName", "findAllByBatchId");
 		SimpleNoteListRequest.addProperty("batchId", batchId);
-		List<?> SimpleNoteList = (List<?>) rabbitTemplate.convertSendAndReceive(RABBIT_REPO_EXCHANGE,
-				LIST_NOTE_ROUTING_KEY, SimpleNoteListRequest.toString());
-		return SimpleNoteList.stream().map(x->(SimpleNote) x).collect(Collectors.toList());
+		// List<?> SimpleNoteList = (List<?>) rabbitTemplate.convertSendAndReceive(RABBIT_REPO_EXCHANGE,
+		// 		LIST_NOTE_ROUTING_KEY, SimpleNoteListRequest.toString());
+		return null;//SimpleNoteList.stream().map(x->(SimpleNote) x).collect(Collectors.toList());
 	}
 	/**
 	 * 
@@ -101,7 +95,7 @@ public class BatchCompositionMessageService {
 		JsonObject TraineeDeleteRequest = new JsonObject();
 		TraineeDeleteRequest.addProperty("methodName", "delete");
 		TraineeDeleteRequest.addProperty("batchId", batchId);
-		rabbitTemplate.convertSendAndReceive(RABBIT_REPO_EXCHANGE, LIST_TRAINEE_ROUTING_KEY, TraineeDeleteRequest.toString());
+		// rabbitTemplate.convertSendAndReceive(RABBIT_REPO_EXCHANGE, LIST_TRAINEE_ROUTING_KEY, TraineeDeleteRequest.toString());
 		
 	}
 }
