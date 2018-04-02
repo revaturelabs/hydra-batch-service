4493c755624009d815d132101f4551f7b0afd06b
Bryton Hall <email@bryton.io>
2018-04-01 23:41:38 -0400

    seperate documentation configuration from main class

--- src/main/java/com/revature/hydra/batch/BatchRepositoryServiceApplication.java
+++ src/main/java/com/revature/hydra/batch/BatchRepositoryServiceApplication.java
@@ -1,40 +1,17 @@
 package com.revature.hydra.batch;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
-
-import com.revature.hydra.batch.service.BatchCompositionMessageService;
-import com.revature.hydra.batch.service.BatchCompositionService;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableSwagger2
 @EnableDiscoveryClient 
+@ComponentScan
 public class BatchRepositoryServiceApplication {
-
-	@Autowired
-	BatchCompositionMessageService bcms;
-
-	@Autowired
-	BatchCompositionService bcs;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BatchRepositoryServiceApplication.class, args);
 	}
 
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.revature.hydra.batch.controller"))
-				.paths(PathSelectors.any()).build();
-	}
-	
 }
