5a604f67a7eb475146c6fcd60c3001362b56e4dd
Robert Hamel <roberthamel@gmail.com>
2018-04-06 10:43:37 -0400

    configure jwt tokens

--- src/main/java/com/revature/hydra/batch/security/SecurityConfig.java
+++ src/main/java/com/revature/hydra/batch/security/SecurityConfig.java
@@ -1,18 +1,28 @@
 package com.revature.hydra.batch.security;
 
+import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value(value = "${AUTH0_SECRET}")
+    private String clientSecret;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
+        JwtWebSecurityConfigurer
+            .forRS256("hydra-gateway", "https://revature.auth0.com/")
+            .configure(http)
+            .authorizeRequests()
             .anyRequest().authenticated()
             .and()
-            .addFilter(new JWTAuthorizationFilter(authenticationManager()))
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
