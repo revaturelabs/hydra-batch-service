2a0729a053e871c4e1494bebfbe6c322f42e4608
Robert Hamel <roberthamel@gmail.com>
2018-04-06 11:19:16 -0400

    remove unnecessary import

--- src/main/java/com/revature/hydra/batch/security/SecurityConfig.java
+++ src/main/java/com/revature/hydra/batch/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.revature.hydra.batch.security;
 
 import com.auth0.spring.security.api.JwtWebSecurityConfigurer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
