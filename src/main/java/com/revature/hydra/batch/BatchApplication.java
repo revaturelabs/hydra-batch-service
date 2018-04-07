f7b70505e7094099e3cac94751f50a24d98b6b26
Bryton Hall <email@bryton.io>
2018-04-07 10:38:11 -0400

    update dependencies and application profiles

--- src/main/java/com/revature/hydra/batch/BatchApplication.java
+++ src/main/java/com/revature/hydra/batch/BatchApplication.java
@@ -3,13 +3,10 @@ package com.revature.hydra.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -23,8 +20,4 @@ public class BatchApplication {
 		SpringApplication.run(BatchApplication.class, args);
 	}
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2);
-    }
 }
