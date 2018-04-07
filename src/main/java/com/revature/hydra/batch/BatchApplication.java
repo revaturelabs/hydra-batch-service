a63b5b1ec070a3099606948a27d45111c7204969
Robert Hamel <roberthamel@gmail.com>
2018-04-07 15:12:18 -0400

    fix compile problem

--- src/main/java/com/revature/hydra/batch/BatchApplication.java
+++ src/main/java/com/revature/hydra/batch/BatchApplication.java
@@ -1,15 +1,15 @@
 package com.revature.hydra.batch;
 
-import com.revature.hydra.batch.model.Batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -19,9 +19,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Import(SpringDataRestConfiguration.class)
 public class BatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BatchApplication.class, args);
+    }
 
     @Bean
     public Docket api() {
